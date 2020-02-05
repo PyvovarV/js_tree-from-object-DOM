91b1c07cffa06002e11637280d8f25959568ba87
Vova Pyvovar <ditinapivo@gmail.com>
2020-02-05 17:07:50 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+    createTree(ul, data[key]);
+  }
+}
+
+createTree(tree, food);
